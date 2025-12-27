d6329b6bc7767b9c690ce1e1dae8dd3ec4cbc6ae
MENA-dev1 <menatube0721@gmail.com>
2025-12-27 19:21:07 +0900

    Update MainGame.h

--- MainGame.h
+++ MainGame.h
@@ -2,9 +2,14 @@
 class MainGame
 {
 private:
-	int gold;
-	int stage;
+	int Coin;
+	int CardBox;
+	int LargeStage;
+	int SmallStage;
+
 public:
+	int mx, my;	// 마우스 커서
+
 	MainGame();
 	void AddGold(int v);
 	void RemoveGold(int v);
