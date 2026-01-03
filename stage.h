518da63ac7230bb1c0d42f93513bb98768ea44ce
Cr1Ganghunu <gh.dev1of.github@gmail.com>
2026-01-03 23:32:27 +0900

    Update stage.h

--- stage.h
+++ stage.h
@@ -1,26 +1,39 @@
 #pragma once
 #include "item.h"
-#include "card.h"
+#include "Card.h"
 #include "MainGame.h"
 using namespace std;
 
-class Stage : Card
+class Stage : MainGame
 {
-private:
-	int largeStage;
-	int smallStage; 
-	MainGame* mainGame;
+protected:
+	int L_stage; //월드
+	int S_stage; //스테이지
+	MainGame* Game;
 public:
-	bool IsBossStage(const MainGame& game) ;
+	bool IsBossStage(const MainGame& game);
 	bool GameStart();
-	bool StageClear(bool isMonsterDead, MainGame& game);
+	bool StageClear(bool isMonsterDead);
 	bool GameOver(bool isPlayerDead);
-	bool TutorialStart();
+	//bool CheckTutorial();
 	bool StageStart();
 	bool GameClear();
+	//int GL_Stage(MainGame& game);
+	//int GS_Stage(MainGame& game);
+
 	//int getstage();
 	int setgold();
-	CType TutorialCard();
-	void Init(int large, int small);
+	
 
+};
+
+
+class TutorialStage : Stage //다중 상속?
+{
+private:
+	bool IsTutorial;
+
+public:
+	CType TutorialCard();
+	bool CheckTutorial();
 };
