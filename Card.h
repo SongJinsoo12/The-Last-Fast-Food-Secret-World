4847c9a79e09690ccd36e7a04429a350b0e10da5
SongJinsoo12 <jinsoo318@gmail.com>
2026-01-03 15:55:57 +0900

    Update Card.h
    Deck 클래스 추가

--- Card.h
+++ Card.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <vector>
+using namespace std;
+
 enum CAttribute {
 	E_BULGOGI,
 	E_SOURCE,
@@ -23,6 +26,29 @@ protected:
 
 public:
 	Card();
+	//카드 위치
+	int x;
+	int y;
+	////드래그
+	//bool drag;
 };
 
+//덱 및 패
+class Deck
+{
+public:
+	Deck();
+	~Deck();
+
+	int GetDeckCount();
+	int GetHandCount();
+	vector<Card> GetHand();
+	void CardDraw();
+	bool TurnEnd();
+
+private:
+	int deckCount;//덱 장수
+	int handCount;//패 장수
+	vector<Card> hand; //패 카드
+};
 
