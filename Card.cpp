75a4f45b2ef9086e4497a3a1245749ba3012acfa
SongJinsoo12 <jinsoo318@gmail.com>
2026-01-03 15:55:28 +0900

    Update Card.cpp
    Deck 클래스 추가

--- Card.cpp
+++ Card.cpp
@@ -5,4 +5,51 @@ Card::Card() {
 	Rdc = 0;
 	Ait = E_BREAD;
 	Type = E_Attack;
+	x = 0;
+	y = 0;
+}
+
+Deck::Deck() : deckCount(25), handCount(5) // 패 임시 장수
+{
+	Card temp;
+	for (size_t i = 0; i < handCount; i++)
+		hand.push_back(temp);
+}
+
+Deck::~Deck()
+{
+}
+
+int Deck::GetDeckCount()
+{
+	return deckCount;
+}
+
+int Deck::GetHandCount()
+{
+	return handCount;
+}
+
+vector<Card> Deck::GetHand()
+{
+	return hand;
+}
+
+//드로우
+void Deck::CardDraw()
+{
+	if (deckCount <= 0)
+		return;
+
+	deckCount--;
+	handCount++;
+
+	Card temp; //임시
+	hand.push_back(temp);
+}
+
+//턴 엔드
+bool Deck::TurnEnd()
+{
+	return true;
 }
