36719506d83b126237b561cb873d9f38420b65ab
SongJinsoo12 <jinsoo318@gmail.com>
2026-01-04 16:16:44 +0900

    Delete CardAct.h

--- CardAct.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <Windows.h>
-#include "Card.h"
-
-void DrawLine(HDC hdc, int startX, int startY, int lengthX, int lengthY);
-void DrawBG(HDC hdc, RECT rect);
-void DrawDeckCount(HDC hdc, Deck& playerHand, Deck& enemyHand, RECT rect);
