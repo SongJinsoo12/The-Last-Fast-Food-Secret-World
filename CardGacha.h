82dcfa059199d112026cd3588c35ca57d8e862c1
kimdol2025 <pjammb1594@gmail.com>
2026-01-05 22:52:57 +0900

    Update CardGacha.h

--- CardGacha.h
+++ CardGacha.h
@@ -4,7 +4,7 @@
 #include "macroNum.h"
 #include "Shop.h"
 
-class CardGacha : public Shop
+class CardGacha
 {
 private:
 	vector<Card> draw_card;
@@ -19,12 +19,12 @@ public:
 
 	}
 
-	//ÀüÃ¼Ä«µåÁß 1ÀåÀ» ÀÎº¥¿¡ÀúÀå / »çÀÌÁî´Â Å¬·¡½º³» º¯¼ö·Î ÀúÀåÇØµÎ±â
+	//ì „ì²´ì¹´ë“œì¤‘ 1ì¥ì„ ì¸ë²¤ì—ì €ì¥ / ì‚¬ì´ì¦ˆëŠ” í´ë˜ìŠ¤ë‚´ ë³€ìˆ˜ë¡œ ì €ì¥í•´ë‘ê¸°
 	void one(DeckBuilding& p_deck);
-	//ÀüÃ¼Ä«µå Áß 10ÀåÀ» ÀÎº¥¿¡ÀúÀå
+	//ì „ì²´ì¹´ë“œ ì¤‘ 10ì¥ì„ ì¸ë²¤ì—ì €ì¥
 	void ten(DeckBuilding& p_deck);
 	void GetGacha(bool isOne, DeckBuilding& p_deck);
 
-	void DrawGacha(HDC p_hdc, DeckBuilding p_deck, HPEN p_hpen, HPEN p_oldpen, int p_mx, int p_my, WCHAR p_text[]);
+	void DrawGacha(HDC p_hdc, DeckBuilding p_deck, int p_price, HPEN p_hpen, HPEN p_oldpen, int p_mx, int p_my, WCHAR p_text[]);
 };
 
