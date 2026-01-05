1a5730226b6f56dbfe61812b7a24e5963b7f0115
SongJinsoo12 <jinsoo318@gmail.com>
2026-01-05 19:56:42 +0900

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <vector>
 #include <Windows.h>
+#include <random>
 using namespace std;
+
+#define TURNTIME 777
+#define randomInit(start, end)  \
+	random_device rd; \
+	mt19937 gen(rd()); \
+	uniform_int_distribution<int> cookRandom(start, end)
 
 enum CAttribute {
 	E_BULGOGI,
@@ -43,18 +50,20 @@ public:
 	int GetHandCount();
 	vector<Card> GetHand();
 	void CardDraw();
-	bool TurnEnd();
-	void CardAct();
+	void CardAct(CardManager& opponent, HWND hWnd);
 
 	void DrawLine(HDC hdc, int startX, int startY, int lengthX, int lengthY);
 	void DrawBG(HDC hdc, RECT rect, int cardX, int cardY);
 	void DrawDeckCount(HDC hdc,  int rtX, int rtY, int cardX, int cardY);
-	void DrawHand(HDC hdc, int rtX, int rtY, int cardX, int cardY);
-	void HandSelect(WPARAM wParam);
+	void DrawHand(HDC hdc, int rtX, int rtY, int cardX, int cardY, bool isPlayer);
+	void HandSelect(WPARAM wParam, CardManager& opponent, HWND hWnd);
+	void StartTurn(CardManager& player, CardManager& opponent);
+	void TimeLimit(WPARAM wParam, CardManager& opponent, HWND hWnd, LPCWSTR text, LPCWSTR caption, UINT type);
 
 private:
 	unsigned int deckCount;//덱 장수
 	unsigned int handCount;//패 장수
 	int handSelection;//패 카드 선택
-	vector<Card> hand; //패 카드
+	vector<Card> hand;//패 카드
+	bool isMyTurn;//턴 확인
 };
