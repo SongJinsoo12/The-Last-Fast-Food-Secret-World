9f08670e03f04c11482693a30ffafcecff7a5139
SongJinsoo12 <jinsoo318@gmail.com>
2026-01-06 13:15:46 +0900

    Create CardTableManager.h

---
+++ CardTableManager.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Card.h"
+#include <vector>
+#include <random>
+using namespace std;
+
+
+class CardTableManager
+{
+
+protected:
+	static CardTableManager* m_Instance;
+public:
+	static CardTableManager* Instnace();
+
+public:
+	CardTableManager();
+	virtual ~CardTableManager();
+
+	void Init();
+
+	Card* GetCardData(int p_uid);
+	Card* GetCardData(ALLCARDEnum p_uid);
+
+
+	vector<Card*> GetRandomCard(int p_count);
+
+protected:
+	vector<Card*> m_AllCardDataVec;//전체 카드
+
+};
+
