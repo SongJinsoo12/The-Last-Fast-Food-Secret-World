9f04961380dcff6af048436947e0bffbe85625a0
SongJinsoo12 <jinsoo318@gmail.com>
2026-01-10 14:26:35 +0900

    Update Card.h

--- Card.h
+++ Card.h
@@ -21,24 +21,66 @@ enum CAttribute {
 
 enum CType {
 	E_Attack,
-	E_Deffence,
+	E_Deffense,
 	E_Magic
+};
+
+enum ALLCARDEnum
+{
+	BASEATK = 0,
+	ATK01,
+	ATK02,
+	ATKLIMIT = 41,
+
+	BASEDEF = 100,
+	DEF01,
+	DEF02,
+	DEFLIMIT = 144,
+
+	BASEMAGIC = 200,
+	MAGIC01,
+	MAGIC02,
+	MAGICLIMIT = 233,
 };
 
 class Card
 {
 protected:
-	int Atk;
-	int Rdc;
+	int uid;
+	int atk;
+	int def;
 	CAttribute Ait;
 	CType Type;
 
 public:
 	Card();
-	//카드 위치
-	int x;
-	int y;
+	Card(int p_uid);
+	void Init();
+
+	//Get Set 함수
+	int GetUid();
+	void SetUid(int p_uid);
+	int GetAtk();
+	void SetAtk(int p_atk);
+	int GetDef();
+	void SetDef(int p_def);
+	CAttribute GetAit();
+	void SetAit(CAttribute p_Ait);
+	CType GetType();
+	void SetType(CType p_Type);
 };
+
+class GameCard : public Card
+{
+public:
+	GameCard();
+	GameCard(Card* p_Card);
+	virtual ~GameCard();
+
+private:
+
+};
+
 
 //덱 및 패
 class CardManager
@@ -60,10 +102,11 @@ public:
 	void HandSelect(WPARAM wParam, CardManager& opponent, HWND hWnd);
 	void StartTurn(CardManager& player, CardManager& opponent);
 	void TimeLimit(WPARAM wParam, CardManager& opponent);
+	void OpponentAct();
 
 private:
-	unsigned int deckCount;//덱 장수
-	unsigned int handCount;//패 장수
+	int deckCount;//덱 장수
+	int handCount;//패 장수
 	int handSelection;//패 카드 선택
 	vector<Card> hand;//패 카드
 	bool isMyTurn;//턴 확인
