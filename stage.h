e1a82770fc5db1a0a91c913e2aff1b0330ace1ab
Cr1Ganghunu <gh.dev1of.github@gmail.com>
2026-01-18 23:38:59 +0900

    Refactor Stage and TutorialStage classes in stage.h

--- stage.h
+++ stage.h
@@ -15,33 +15,32 @@ enum StageState
 class Stage : MainGame
 {
 protected:
-	//int L_Stage; //월드
-	//int S_Stage; //스테이지
+	
 	
 public:
-	int StageState;
-	bool IsBossStage();
-	bool IsLastBossStage();
+	int StageState; //스테이지 상태
+	bool IsBossStage(); //보스 스테이지 확인
+	bool IsLastBossStage(); //막보 스테이지 확인
 
-	bool GameStart();
-	bool GameOver(bool isPlayerLost);
+	bool GameStart(); //게임 시작
+	bool GameOver(bool isPlayerLost); //게임 오버
 
 	//bool CheckTutorial();
-	bool StageStart();
-	int StageClear(bool isMonsterLost);
+	bool StageStart(); //스테이지 시작
+	int StageClear(bool isMonsterLost); //스테이지 클리어
 
-	bool IsGameClear();
-	int NextStage();
+	bool IsGameClear(); //게임 클리어 확인
+	int NextStage(); //다음 스테이지 
 	
-	int Get_LargeStage();
-	int Get_SmallStage();
-	//int getstage();
-	int setgold();
-	
- 
+	int Get_LargeStage(); //튜토리얼 클래스용
+	int Get_SmallStage(); //튜토리얼 클래스용
+	int setgold(); //스테이지에 따라 골드 설정
+	void DropCard(/* */); //스테이지 클리어 후 카드 드랍
+	//void Draw StageClearScreen(); //스테이지 클리어 화면 출력
+
 };
 
-enum TCard
+enum TCardType
 {
 	E_TUTORIAL_ATTACK,
 	E_TUTORIAL_DEFENSE,
@@ -55,7 +54,6 @@ private:
 
 public:
 	 
-	TCard TutorialCard();
-	bool CheckTutorial();
+	TCardType TutorialCard(); //튜토리얼 스테이지에 맞는 카드 반환
+	bool CheckTutorial(); //튜토리얼 스테이지 확인
 };
-
