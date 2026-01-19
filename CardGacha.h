25e7f8d5742d78ac37e0eed767960abc6d86e6ff
kimdol2025 <pjammb1594@gmail.com>
2026-01-20 00:12:16 +0900

    Add isOneGacha member variable to CardGacha class

--- CardGacha.h
+++ CardGacha.h
@@ -11,6 +11,7 @@ private:
 	int gold_out;
 	vector<Card> draw_card;
 	bool isGachaFailed = false;
+	bool isOneGacha;
 	int index = 0;
 	//전체카드풀
 	GameCard* allCard[AllCARDMAXSIZE];
@@ -40,11 +41,10 @@ public:
 
 	}
 
-	void Sort(); //중복카드 제거 및 id순서대로 정렬
-	void one(DeckBuilding& p_deck, MainGame& p_mg);	//전체카드 중 1장을 인벤에저장
-	void ten(DeckBuilding& p_deck, MainGame& p_mg);	//전체카드 중 10장을 인벤에저장
-	void GetGacha(bool isOne, DeckBuilding& p_deck, MainGame& p_mg, Chest p_selChest); //뽑기를 실행(T-1/F-10)
-	void InGacha(); //뽑기화면 속 로직
+	void one(DeckBuilding& p_deck, MainGame& p_mg);//전체카드 중 1장을 인벤에저장
+	void ten(DeckBuilding& p_deck, MainGame& p_mg);//전체카드 중 10장을 인벤에저장
+	void GetGacha(bool isOne, DeckBuilding& p_deck, MainGame& p_mg, Chest p_selChest);//뽑기를 실행(T-1/F-10)
+	void InGacha();//뽑기화면 속 로직
 
 	//상점에 뽑기버튼을 출력, 버튼 클릭 시 뽑기를 실행
 	void DrawGachaButton(HDC p_hdc, DeckBuilding p_deck, Chest p_selChest, HPEN p_hpen, HPEN p_oldpen, int p_mx, int p_my, WCHAR p_text[]);
