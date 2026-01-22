8ffc6b84f93f3b685721c919226b9d1796549297
MENA-dev1 <menatube0721@gmail.com>
2026-01-22 15:47:19 +0900

    Enhance RenderManager with layer management features
    Added LayerType enum and updated RenderManager to support layers.

--- RenderManager.h
+++ RenderManager.h
@@ -4,28 +4,45 @@
 #include "ImageManager.h"
 #include "Singleton.h"
 
+#include <unordered_map>
+
 namespace GameImage_M {
+
+	enum class LayerType {
+		Background=0,
+		Field,
+		Card,
+		UI,
+		Max,
+	};
 
 	class RenderManager : public Singleton<RenderManager>
 	{
 		friend class Singleton<RenderManager>;
 
 	private:
-		vector<shared_ptr<ImageLoad>> m_RenderList;
-
-	//private:
-	//	virtual ~RenderManager();
+		vector<vector<shared_ptr<ImageLoad>>> m_RenderList;
+		unordered_map<string, LayerType> m_IDMap;
 
 	public:
+		RenderManager();
 		virtual ~RenderManager();
 
 	public:
-		void SetImage(wstring p_path, string p_id, Rect load, Rect render, bool isVisible);
+		void SetImage(wstring p_path, string p_id, Rect load, Rect render
+			, bool isVisible, LayerType p_layer);
 		void RenderAll(Graphics* grap);
 
 		void ImageVisible(string p_id, bool isVisible);
 		void MoveImage(string p_id, Rect renderPos);
 		void RemoveIDIamage(string p_id);
 		void AllRemoveImage();
+
+		shared_ptr<ImageLoad> FindImageID(string p_id);
+
+		void LayerMoveForward(string p_id);
+		void LayerMoveBackward(string p_id);
+		void LayerMoveToFont(string p_id);
+		void LayerMoveFoBack(string p_id);
 	};
 }
