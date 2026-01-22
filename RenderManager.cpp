0831d55b54d13ac6089de710d72bb142b8a2b268
MENA-dev1 <menatube0721@gmail.com>
2026-01-22 15:47:04 +0900

    Enhance RenderManager with layered image handling
    Refactor RenderManager to support layered rendering and improve image management.

--- RenderManager.cpp
+++ RenderManager.cpp
@@ -2,75 +2,155 @@
 #include <iostream>
 
 namespace GameImage_M {
-
+	RenderManager::RenderManager()
+	{
+		// 사이즈 설정
+		m_RenderList.resize(static_cast<int>(LayerType::Max));
+	}
 	RenderManager::~RenderManager()
 	{
 		AllRemoveImage();
 		ImageManager::GetInstance()->Release();
 	}
-	void RenderManager::SetImage(wstring p_path, string p_id, Rect load, Rect render, bool isVisible)
+
+	void RenderManager::SetImage(wstring p_path, string p_id, Rect load, Rect render
+		, bool isVisible, LayerType p_layer)
 	{
-		for (vector<shared_ptr<ImageLoad>>::iterator it = m_RenderList.begin();
-			it != m_RenderList.end(); )
-		{
-			if ((*it)->GetID() == p_id) {
-				cout << "ID 중복 값 존재" << endl;
-				return;
-			}
-			else
-				++it;
-		}
+		if (m_IDMap.find(p_id) != m_IDMap.end()) return;
 
 		shared_ptr<ImageLoad> newImage = make_shared<ImageLoad>();
 		newImage->LoadI(p_path, p_id, load, render, isVisible);
-		m_RenderList.push_back(newImage);
+
+		m_RenderList[(int)p_layer].push_back(newImage);
+		m_IDMap[p_id] = p_layer;
 	}
+
 	void RenderManager::RenderAll(Graphics* grap)
 	{
-		for (size_t i = 0; i < m_RenderList.size(); i++)
+		for (int i = 0; i < (int)LayerType::Max; i++)
 		{
-			if (m_RenderList[i].get()->GetVisible())
+			vector<shared_ptr<ImageLoad>>& currentLayer = m_RenderList[i];
+			for (int j = 0; j < (int)currentLayer.size(); j++)
 			{
-				shared_ptr<ImageLoad> p_Image = m_RenderList[i];
-				if (p_Image != nullptr) {
-					p_Image->Render(grap);
+				if (currentLayer[j]->GetVisible()) {
+					currentLayer[j]->Render(grap);
 				}
 			}
 		}
 	}
+
 	void RenderManager::ImageVisible(string p_id, bool isVisible)
 	{
-		for (size_t i = 0; i < m_RenderList.size(); i++)
-		{
-			if (m_RenderList[i]->GetID() == p_id) {
-				m_RenderList[i]->IsVisibleImage(isVisible);
-				break;
-			}
-		}
+		shared_ptr<ImageLoad> img = FindImageID(p_id);
+		if (img) img->IsVisibleImage(isVisible);
 	}
+
 	void RenderManager::MoveImage(string p_id, Rect renderPos)
 	{
-		for (size_t i = 0; i < m_RenderList.size(); i++)
-		{
-			if (m_RenderList[i]->GetID() == p_id) {
-				m_RenderList[i]->Move(renderPos);
-				break;
-			}
-		}
+		shared_ptr<ImageLoad> img = FindImageID(p_id);
+		if (img) img->Move(renderPos);
 	}
+
 	void RenderManager::RemoveIDIamage(string p_id)
 	{
-		for (size_t i = 0; i < m_RenderList.size(); i++)
+		if (m_IDMap.find(p_id) == m_IDMap.end()) return;
+
+		LayerType layer = m_IDMap[p_id];
+		vector<shared_ptr<ImageLoad>>& targetVec = m_RenderList[(int)layer];
+
+		for (vector<shared_ptr<ImageLoad>>::iterator it= targetVec.begin(); 
+			it != targetVec.end(); it++)
 		{
-			if (m_RenderList[i]->GetID() == p_id) {
-				m_RenderList.erase(m_RenderList.begin() + i);
+			if ((*it)->GetID()==p_id){
+				targetVec.erase(it);
+				m_IDMap.erase(p_id);
 				break;
 			}
 		}
 	}
+
 	void RenderManager::AllRemoveImage()
 	{
+		for (int i = 0; i < (int)LayerType::Max; i++)
+		{
+			m_RenderList[i].clear();
+		}
 		m_RenderList.clear();
 		m_RenderList.shrink_to_fit();
+		m_IDMap.clear();
+	}
+
+	shared_ptr<ImageLoad> RenderManager::FindImageID(string p_id)
+	{
+		if (m_IDMap.find(p_id) == m_IDMap.end()) return;
+
+		LayerType layer = m_IDMap[p_id];
+		vector<shared_ptr<ImageLoad>>& targetVec = m_RenderList[(int)layer];
+		for (const shared_ptr<ImageLoad>& img : targetVec)
+		{
+			if (img->GetID() == p_id) return img;
+		}
+
+		return nullptr;
+	}
+	void RenderManager::LayerMoveForward(string p_id)
+	{
+		if (m_IDMap.find(p_id) == m_IDMap.end()) return;
+		vector<shared_ptr<ImageLoad>>& targetVec = m_RenderList[(int)m_IDMap[p_id]];
+
+		for (int i = 0; i < (int)targetVec.size() - 1; i++)
+		{
+			if (targetVec[i]->GetID() == p_id) {
+				swap(targetVec[i], targetVec[i + 1]);
+				break;
+			}
+		}
+	}
+	void RenderManager::LayerMoveBackward(string p_id)
+	{
+		if (m_IDMap.find(p_id) == m_IDMap.end()) return;
+		vector<shared_ptr<ImageLoad>>& targetVec = m_RenderList[(int)m_IDMap[p_id]];
+
+		for (int i = 0; i < (int)targetVec.size() - 1; i++)
+		{
+			if (targetVec[i]->GetID() == p_id) {
+				swap(targetVec[i], targetVec[i - 1]);
+				break;
+			}
+		}
+	}
+	void RenderManager::LayerMoveToFont(string p_id)
+	{
+		if (m_IDMap.find(p_id) == m_IDMap.end()) return;
+		LayerType layer = m_IDMap[p_id];
+		vector<shared_ptr<ImageLoad>>& targetVec = m_RenderList[(int)layer];
+
+		for (vector<shared_ptr<ImageLoad>>::iterator it=targetVec.begin();
+			it != targetVec.end(); it++)
+		{
+			if ((*it)->GetID() == p_id) {
+				shared_ptr<ImageLoad> temp = *it;
+				targetVec.erase(it);
+				targetVec.push_back(temp);
+				break;
+			}
+		}
+	}
+	void RenderManager::LayerMoveFoBack(string p_id)
+	{
+		if (m_IDMap.find(p_id) == m_IDMap.end()) return;
+		LayerType layer = m_IDMap[p_id];
+		vector<shared_ptr<ImageLoad>>& targetVec = m_RenderList[(int)layer];
+
+		for (vector<shared_ptr<ImageLoad>>::iterator it = targetVec.begin();
+			it != targetVec.end(); it++)
+		{
+			if ((*it)->GetID() == p_id) {
+				shared_ptr<ImageLoad> temp = *it;
+				targetVec.erase(it);
+				targetVec.insert(targetVec.begin(), temp);
+				break;
+			}
+		}
 	}
 }
