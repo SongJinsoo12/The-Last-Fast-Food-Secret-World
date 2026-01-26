732e7b2a4d583b4ea096d285811c598ccc17821a
kimdol2025 <pjammb1594@gmail.com>
2026-01-27 02:08:38 +0900

    Update DeckBuilding.h

--- DeckBuilding.h
+++ DeckBuilding.h
@@ -6,10 +6,10 @@
 #include <Windows.h>
 #include "Card.h"
 #include "macroNum.h"
+#include "RenderManager.h"
 
 #include <iostream>
 #include <fstream>	//파일읽기
-#include <map>		//키-키값
 #include "json.hpp"
 
 using namespace std;
@@ -26,71 +26,38 @@ private:
 	vector<Card> myDeck;
 	//보유한 카드들
 	vector<Card> inven;
+	Card* SelectedCard;//덱편집화면에서 유저가 정보를 확인할 카드변수 / exit시 선택해제할것
 
 	int Star_n[3] = { 0, };//덱의 1,2,3성 카드개수
+	int i_page = 0, max_page = 0;
 
 public:
 	DeckBuilding()
 	{
-
+		
 	}
-
 	virtual ~DeckBuilding()
 	{
-
 	}
 
-	void SaveDeck()
+	void SaveDeck();
+	void LoadDeck();
+
+	void PageBuff(bool p_isIncrease)
 	{
-		// 1. JSON 객체 생성 및 데이터 추가
-		json j;
-		j[]
-		for (int i = 0; i < myDeck.size(); i++)
+		if (p_isIncrease)
 		{
-			int id = myDeck[i].GetUid();
-			j["ID" + to_string(i)] = to_string(id);
-			cout << to_string(i) << "번째 카드저장완료" << endl;
+			++i_page;
+			if (i_page > max_page) --i_page;
 		}
-
-		// 2. 파일 스트림 열기 (output.json 파일)
-		std::ofstream outfile("Deck.json");
-
-		// 3. JSON 객체를 파일 스트림에 출력 (직렬화)
-		if (outfile.is_open()) {
-			outfile << j << std::endl; // << 연산자로 파일에 쓰기
-			outfile.close();
-			std::cout << "JSON 데이터가 output.json 파일에 저장되었습니다." << std::endl;
-		}
-		else {
-			std::cerr << "파일 열기 실패!" << std::endl;
-		}
-	}
-
-	void LoadDeck()
-	{
-		std::ifstream file("Deck.json", std::ios::in);//같은 경로내에서 파일불러오기
-
-		//파일 불러오기확인용
-		if (!file.is_open())
+		else
 		{
-			std::cerr << "Error opening Deck.txt.\n";
-			return;
+			--i_page;
+			if (i_page < 0) i_page = 0;
 		}
-
-		//읽어들인 파일을 json에 저장
-		json j;
-		file >> j;
-
-		//map배열에 불러온 키와 키값을 저장함
-		std::map<std::string, std::string> Card;
-		for (const auto& result : j["results"])
-			Card[result["ID"].get<std::string>()] = result["Value"].get<std::string>();
-
-		//first == 키이름, second == 키값
-		for (const auto& pair : Card)
-			std::cout << pair.first << " : " << pair.second << "\n";
+		cout << i_page << " / " << max_page << endl;
+		return;
 	}
-
 	//중복이 존재하면 제거후 뒤의 카드들을 앞으로 이동
 	vector<Card> EraseDuple(vector<Card> p_cards);
 
@@ -104,14 +71,24 @@ public:
 	void ItoD(int p_mx, int p_my);
 	//덱->인벤 (덱, 마우스X, 마우스Y)
 	void DtoI(int p_mx, int p_my);
-	void DeckBuild(int p_mx, int p_my);
+	void SelectCard(int p_mx, int p_my);
+	void DeckBuild(int p_mx, int p_my, char click_m);//click_m == 좌/우클릭 확인용, 좌-카드하이라이트/우-카드이동
 
 	//인벤토리 출력
-	void DrawInventory(HDC p_hdc, HPEN p_hpen, HPEN p_oldpen, int p_mx, int p_my, WCHAR p_text[]);
+	void DrawInventory(HDC p_hdc, WCHAR p_text[]);
 	//마이덱 출력
-	void DrawMyDeck(HDC p_hdc, HPEN p_hpen, HPEN p_oldpen, int p_mx, int p_my, WCHAR p_text[]);
+	void DrawMyDeck(HDC p_hdc, WCHAR p_text[]);
 	//덱빌딩 화면 출력
-	void DrawDeckBuild(HDC p_hdc, HPEN p_hpen, HPEN p_oldpen, int p_mx, int p_my, WCHAR p_text[]);
-};
+	void DrawDeckBuild(HDC p_hdc, WCHAR p_text[]);
 
-extern DeckBuilding g_DeckBuilding;
+	void EnterDeckBuild()
+	{
+		max_page = (inven.size() / 25);
+		cout << "maxpage : " << max_page << endl;
+	}
+
+	void ExitDeckBuild()
+	{
+
+	}
+};
