f37be26b8f29d1f750d50be10988fef2c20d0cc1
kimdol2025 <pjammb1594@gmail.com>
2026-01-27 02:09:03 +0900

    Update Card.h

--- Card.h
+++ Card.h
@@ -3,9 +3,12 @@
 #include <Windows.h>
 #include <random>
 #include <iostream>
+
 using namespace std;
 
 #define TURNTIME 777
+#define CARDX 100
+#define CARDY 132
 #define randomInit(start, end)  \
 	random_device rd; \
 	mt19937 gen(rd()); \
@@ -43,6 +46,13 @@ enum ALLCARDEnum
 	MAGICLIMIT = 233,
 };
 
+enum Star
+{
+	E_ONE = 0,
+	E_TWO,
+	E_THREE,
+};
+
 class Card
 {
 protected:
@@ -51,6 +61,7 @@ protected:
 	int def;
 	CAttribute Ait;
 	CType Type;
+	Star star;
 
 public:
 	int x, y;
@@ -69,6 +80,9 @@ public:
 	void SetAit(CAttribute p_Ait);
 	CType GetType();
 	void SetType(CType p_Type);
+	Star GetStar();
+	void SetStar(Star p_Star);
+
 };
 
 class GameCard : public Card
@@ -88,30 +102,33 @@ class CardManager
 {
 public:
 	CardManager();
-	void StartGame(vector<GameCard*> deck);
+	void StartGame();
 	~CardManager();
 
 	int GetDeckCount();
 	int GetHandCount();
 	vector<GameCard*> GetHand();
-	void CardDraw(vector<GameCard*> deck, int drawNum);
+	void SetDeck();
+	void CardDraw(int drawNum);
 	void CardAct(CardManager& opponent, HWND hWnd);
 
 	void DrawLine(HDC hdc, int startX, int startY, int lengthX, int lengthY);
-	void DrawBG(HDC hdc, RECT rect, int cardX, int cardY);
-	void DrawDeckCount(HDC hdc, int rtX, int rtY, int cardX, int cardY);
-	void DrawHand(HDC hdc, int rtX, int rtY, int cardX, int cardY, bool isPlayer);
+	void DrawBG();
+	void DrawPlayerHand();
+	void DrawOppHand();
 	void HandSelect(WPARAM wParam, CardManager& opponent, HWND hWnd);
 	void StartTurn(CardManager& player, CardManager& opponent);
-	void TimeLimit(WPARAM wParam, CardManager& opponent, vector<GameCard*> deck);
+	void TimeLimit(WPARAM wParam, CardManager& opponent);
 	void OpponentAct();
+
+	void SetImage();
 
 private:
 	int deckCount;//덱 장수
 	int handCount;//패 장수
 	int handSelection;//패 카드 선택
 	vector<GameCard*> hand;//패 카드
+	vector<GameCard*> deck;//덱 카드
 	bool isMyTurn;//턴 확인
+	bool isSelect;//패 카드 선택 확인
 };
-
-void FreeMemory(vector<GameCard*> vec);
