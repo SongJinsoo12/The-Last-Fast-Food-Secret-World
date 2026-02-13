f676e8e45fa2c2b77c4d4b7bfdd0a2308ce1063a
SongJinsoo12 <jinsoo318@gmail.com>
2026-02-13 19:21:10 +0900

    ....

--- plz_work/GameMenu.cpp
+++ plz_work/GameMenu.cpp
@@ -49,7 +49,7 @@ void GameMenu::Logic()
 
 	if (ButtonMenu.HandleClickId(mx, my) == "Stage" 
 		&& INPUT.isOneClick(GameInput_M::MouseValue::Left)) {
-		STATE.ChangeState(GameState_M::E_InGameState::InGame);
+		STATE.ChangeState(GameState_M::E_InGameState::Stage);
 	}
 	if (ButtonMenu.HandleClickId(mx, my) == "DeckBuild"
 		&& INPUT.isOneClick(GameInput_M::MouseValue::Left)) {
