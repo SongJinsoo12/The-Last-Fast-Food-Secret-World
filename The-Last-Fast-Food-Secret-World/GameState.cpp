85b39ceea7d98064d35f1dcd30000fa8e28610b1
MENA-dev1 <menatube0721@gmail.com>
2026-02-13 13:29:03 +0900

    Refactor GameState to improve structure and readability

--- The-Last-Fast-Food-Secret-World/GameState.cpp
+++ The-Last-Fast-Food-Secret-World/GameState.cpp
@@ -5,7 +5,8 @@
 #include <chrono>
 
 //각 화면
- 
+#include "GameLobby.h"
+#include "GameMenu.h" 
 #include "Shop.h"
 #include "DeckBuilding.h"
 #include "CardGacha.h"
@@ -21,6 +22,7 @@ namespace GameState_M {
 		StateVector.resize((int)E_InGameState::InGameResult + 1);
 		StateVector[(int)E_InGameState::Lobby] = make_shared<Lobby>();
 		StateVector[(int)E_InGameState::Menu] = make_shared<Menu>();
+		StateVector[(int)E_InGameState::Stage] = make_shared<Stage>();
 		StateVector[(int)E_InGameState::DeckBuild] = make_shared<DeckBuild>();
 		StateVector[(int)E_InGameState::Shop] = make_shared<Shop>();
 		StateVector[(int)E_InGameState::LuckyBox] = make_shared<LuckyBox>();
@@ -47,40 +49,37 @@ namespace GameState_M {
 		currentState.get()->Enter();
 	}
 
+	void Context::Init()
+	{
+		//시작시 덱 로드, 세이브는 소멸자에
+		g_DeckBuild.LoadDeck();
+	}
+
 	void Lobby::Enter()
 	{
-		g_DeckBuild.LoadDeck();//시작시 덱 로드, 세이브는 소멸자에
-		/*M_REND.SetImage(L"test.jpg", "ID_1", Rect(0, 0, 512, 512), Rect(0, 0, 300, 300)
-			, false, GameImage_M::LayerType::Field);*/
+		GameLobby::GetInstance().Enter();
 	}
 
 	void Lobby::Update(HDC p_hdc, HWND p_hwn) {
-		//확인용으로 좌클릭 시 상점이동
-		/*if (GameInput_M::Input::GetInstance().isClick() == (int)GameInput_M::MouseValue::Left)
-			STATE.ChangeState(GameState_M::E_InGameState::Shop);*/
-		//확인용으로 좌클릭 시 스테이지 클리어 결과 이동
-		/*if (GameInput_M::Input::GetInstance().isClick() == (int)GameInput_M::MouseValue::Left)
-			STATE.ChangeState(GameState_M::E_InGameState::InGameResult);*/
-		if (GameInput_M::Input::GetInstance().isClick() == (int)GameInput_M::MouseValue::Left)
-			STATE.ChangeState(GameState_M::E_InGameState::InGame);
+		GameLobby::GetInstance().Logic();
 	}
 
 	void Lobby::Exit() {
-		//M_REND.AllRemoveImage();
+		GameLobby::GetInstance().Exit();
 	}
 
 	void Menu::Enter()
 	{
-		//M_REND.SetImage(L"test.jpg", "ID_1", Rect(0, 0, 512, 512), Rect(100, 0, 300, 300), true, GameImage_M::LayerType::Field);
+		GameMenu::GetInstance().Enter();
 	}
 
 	void Menu::Update(HDC p_hdc, HWND p_hwnd)
 	{
-
+		GameMenu::GetInstance().Logic();
 	}
 
 	void Menu::Exit() {
-		//M_REND.RemoveIDIamage("ID_1");
+		GameMenu::GetInstance().Exit();
 	}
 
 	void DeckBuild::Enter() {
@@ -268,5 +267,6 @@ namespace GameState_M {
 	{
 		g_StageResult.ExitInGameResult();
 	}
+
 
 }
